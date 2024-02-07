8fe63504b041dcb94f87be4d9e758f506082dd92
eseoseodion <ehodion1@sheffield.ac.uk>
2024-02-07 02:59:09 +0000

    Fixed key error using an exception and temperature plot

--- main.py
+++ main.py
@@ -9,28 +9,31 @@ days = st.slider("Forecast days", min_value=1, max_value=5,
                    help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", 
                       ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} in {place}")
+st.subheader(f"{option} for the next {days} days in {place}")
 
 if place: # if place exists as a string/is provided
-    # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
 
-    if option == "Temperature":
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}) # plot a "LINE" graph
-                        # x and y should be array type objects i.e. lists, tuples etc
-                        # x and y should be the same length
-                        # labels should be a dictionary
-        st.plotly_chart(figure) # 
+        if option == "Temperature":
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}) # plot a "LINE" graph
+                            # x and y should be array type objects i.e. lists, tuples etc
+                            # x and y should be the same length
+                            # labels should be a dictionary
+            st.plotly_chart(figure) # 
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
-        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-        image_paths = [images[condition] for condition in sky_conditions]
-        # Render images on the web app
-        print(sky_conditions)
-        st.image(image_paths, width=115)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            # Render images on the web app
+            print(sky_conditions)
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("The city you entered does not exist.")
